66ddb273366edd84e636d77c0ec1ab1e0a05a472
imtiyaj786 <imtiyaj1523@gmail.com>
2021-02-19 11:29:35 +0530

    here i add one js files

---
+++ ind.js
@@ -0,0 +1,11 @@
+/**
+ * This is Created by Mohamad IMTIYAJ ALAM 19/02/2020 FOR MAKE STUDY EASY.
+ */
+
+<script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
+
